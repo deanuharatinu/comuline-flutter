6d37508ad92266e5957c7da679284053f92b763a
deanuharatinu <deanu.alt@gmail.com>
2024-06-06 11:48:56 +0700

    add `exceptions`

---
+++ lib/models/exceptions.dart
@@ -0,0 +1,9 @@
+class NotFoundException implements Exception {}
+
+class ClientException implements Exception {}
+
+class ServerException implements Exception {}
+
+class NoConnectionException implements Exception {}
+
+class UnknownException implements Exception {}
