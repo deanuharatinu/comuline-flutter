dfc7ec4951e38a1f3fd9f3ac3e1f3a226d47cf8d
deanuharatinu <deanu.alt@gmail.com>
2024-06-06 15:19:22 +0700

    add `isar_factory`

---
+++ lib/di/factory/isar_factory.dart
@@ -0,0 +1,26 @@
+import 'package:comuline/data/local/model/station_local.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class IsarFactory {
+  static final IsarFactory _instance = IsarFactory._();
+  factory IsarFactory() => _instance;
+
+  IsarFactory._();
+
+  static Isar? _isar;
+
+  Future<Isar> get isar async {
+    if (_isar != null) return _isar!;
+    _isar = await _initIsar();
+    return _isar!;
+  }
+
+  Future<Isar> _initIsar() async {
+    final dir = await getApplicationDocumentsDirectory();
+    return await Isar.open(
+      [StationLocalSchema],
+      directory: dir.path,
+    );
+  }
+}
